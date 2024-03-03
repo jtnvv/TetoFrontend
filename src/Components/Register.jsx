13ec1c21f822c5354c688c396a17cab7ceddc63b
SergioACV <secastrov@unal.edu.co>
2024-03-03 17:48:38 -0500

    Teto9- Frontend Terminado
    El Frontend del reigstro esta terminado
    Se crearon las funciones para validar y recoger los datos de los usuario. Falta conectar al Backend

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
 
 
 const Register = () =>  {
 
     document.body.style = "background: url('../src/assets/bgRegister.png'); background-size: cover;";
+
+    //Variables del usuario
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+    
+    //Errores
+    const [errors, setError] = useState({
+        
+    })
+
+    //Validación de contraseña y nombre
+
+    const validation =  (values) =>{
+      
+        //Errores
+        const error = {};
+        error.password="";
+        error.name="";
+        error.email="";
+        // Validar que el nombre no esté vacío
+        if (!state.name.trim()) {
+          error.name = "El nombre no puede estar vacío.";
+        }
+
+        // Expresión regular para validar el correo electrónico
+        let emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+        if (!emailRegex.test(state.email)) {
+            error.email= "El correo electrónico no es válido.";
+        }
+
+        // La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        if (!passwordRegex.test(state.password)) {
+            error.password = "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número.";
+        }
+        return error
+        
+  
+      }
+    
+    //capturar datos
+
+    const handleInput = (event) => {
+        
+        setState({
+          ...state,
+          [event.target.name]: event.target.value
+         });
+        
+
+    }
+
+    //Submit al formulario
+
+    const handleSubmit =  (event) => {
+        event.preventDefault();
+        
+        
+        
+        console.log(errors.email)
+        console.log(errors.name)
+        console.log(errors.password)
+
+        //actualizar los errores y validar
+        let errorL = validation(state)
+        setError(errorL)
+
+        
+        if(errorL.name === ""  && errorL.password === "" && errorL.email === ""  ){
+          
+          //añadir función de registro aqui, pues no se ha encontrado errores 
+          alert("CREADO");
+          
+        }else{
+        
+          alert("USUARIO NO CREADO");
+        }
+      }
 
     return (
       
@@ -14,28 +95,32 @@ const Register = () =>  {
           </div>
         
 
-            <form class="space-y-16">
+            <form class="space-y-16" onSubmit={handleSubmit}>
 
                 <div>
                     <label class="block text-gray-700 font-bold mb-2 font-inknut text-left text-lg " style={{fontSize:24}} for="name">
                         Nombre
                     </label>
-                    <input class="block w-96 py-2.3 px-3 text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300  focus_outline-non" id="name" name="name"
+                    <input class="block w-96 py-2.3 px-3 text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300  focus_outline-non" id="name" name="name" onChange={handleInput}
                         type="text"/>
+                    {errors.name && <span className="text-danger text-red-800 text-left block w-96 mt-1 text-sm">{errors.name}</span>}
+                    
                 </div>
                 <div>
                     <label class="block text-gray-700 font-bold mb-2 font-inknut text-left text-lg" style={{fontSize:24}} for="email">
                         Correo Electronico
                     </label>
-                    <input class="block w-96 py-2.3 px-3 text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 apparance-none  focus_outline-non" id="email" name="email"
+                    <input class="block w-96 py-2.3 px-3 text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 apparance-none  focus_outline-non" id="email" name="email" onChange={handleInput}
                         type="email"/>
+                    {errors.email && <span className="text-danger text-red-800 text-left block w-96 mt-1 text-sm">{errors.email}</span>}
                 </div>
                 <div>
                     <label class="block text-gray-700 font-bold mb-2 font-inknut text-left text-lg" style={{fontSize:24}} for="password">
                         Contraseña
                     </label>
-                    <input class="block w-96 py-2.3 px-3 text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 apparance-none focus_outline-non" id="password" name="password"
+                    <input class="block w-96 py-2.3 px-3 text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 apparance-none focus_outline-non" id="password" name="password" onChange={handleInput}
                         type="password"/>
+                    {errors.password && <span className="text-danger text-red-800 text-left block w-96 mt-1 text-sm">{errors.password}</span>}
                 </div>
                 <div >
                     <button class="w-full bg-custom-greyRegister-400 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg mt-10">
