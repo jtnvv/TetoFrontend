49a5dc5e0f92005e637cdfdde648794c82432c57
daperezch <daperezch@unal.edu.co>
2024-03-05 12:01:15 -0500

    62-loggin+Session
    corre loggin y persistencia de sesi[on falta probar que funcione

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
 import { useState } from 'react'
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter, Navigate, Outlet } from "react-router-dom";
+
+import Home from './pages/home'
+import Login from './pages/login'
+import Dashboard from './pages/dashboard'
+import Register from './Components/Register'
+
+import { useSelector } from 'react-redux'
 import './App.css'
 
-import Register from './Components/Register'
+const PrivateRoutes = () => {
+  const { isAuth } = useSelector(state => state.auth)
+  return <>{isAuth ? <Outlet /> : <Navigate to='/login' />}</>
+}
+
+const RestrictedRoutes = () => {
+  const { isAuth } = useSelector(state => state.auth)
+  return <>{!isAuth ? <Outlet /> : <Navigate to='/dashboard' />}</>
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  
+
 
   return (
     <>
-      
-      <Routes>  
-      
-        <Route exact path="/register" element={<Register />} /> 
+      <BrowserRouter>
+        <Routes>
+          {/* aqui entra gente de cualquier tipo menos Abcdo, para Abcedo esta .|. */}
+          <Route path='/' element={<Home />} />
 
-      </Routes>
-      
-    
-      
+          {/* aqui solo hay gente loggeada */}
+          <Route element={<PrivateRoutes />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
+
+          {/* aqui solo gente sin logear */}
+          <Route element={<RestrictedRoutes />}>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
+
     </>
   )
 }
