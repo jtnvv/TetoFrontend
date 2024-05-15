2209fdc4f97ad2810fe0fbec6ccc935c11823f0f
daperezch <daperezch@unal.edu.co>
2024-05-15 14:54:16 -0500

    156-centrarSearch

--- src/pages/search-category.jsx
+++ src/pages/search-category.jsx
@@ -1,13 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import Layout from "../Components/Layout/layout";
 import CardsSearchCategory from "../Components/Search/cards-search-category";
 
 export default function SearchCategory() {
-    const { category } = useParams();
+  const { category } = useParams();
 
-    return (
-        <Layout>
-            <CardsSearchCategory category={category} />
-        </Layout>
-    )
+  return (
+    <div className="flex justify-center mt-3 flex-wrap">
+      <Layout>
+        <CardsSearchCategory category={category} />
+      </Layout>
+    </div>
+  );
 }
