54d3b627761496241a50914d28c0464f9e7de796
SergioACV <secastrov@unal.edu.co>
2024-05-15 16:43:32 -0500

    Bugs arreglados
    Se agregó la cantidad de items en el historial d eordenes. Se hizo la web de perfil responsive. Se agregó un link para redireccionar en el historial de ordenes

--- src/Components/Order/card-order-user.jsx
+++ src/Components/Order/card-order-user.jsx
@@ -5,22 +5,27 @@ const CardOrderUser = (props) => {
 
         <div className="text-white ">
 
-            <div className="flex  bg-[#646458] rounded px-10 py-1 gap-3  items-center">
+            <div className="flex  bg-[#646458] rounded px-10 py-3 gap-3  items-center">
 
-                <div className="mt-2 mb-2 w-2/12">
-                    <img src={props.item.photo} alt="prueba" className="h-20 w-20 object-cover " />
+                <div className="mt-2 mb-2 w-max">
+                    <a key={props.item.id} href={'/product/' + props.item.id}>
+                        <img src={props.item.photo} alt="prueba" className="h-20 w-20 object-cover " />
+                    </a>
                 </div>
 
-                <div className="w-8/12">
+                <div className="w-8/12 mb-3 space-y-1">
                         <p className="text-lg text-white-900 dark:text-white font-semibold ">{props.item.name}</p>
                         <p className="text-sm text-white-900 dark:text-white ">Color: {props.item.order.color}</p>
                         <p className="text-sm text-white-900 dark:text-white ">talla: {props.item.order.size}</p>
+                        <p className="text-sm text-white-900 dark:text-white ">Cantidad: {props.item.order.quantity}</p>
                         <p className="text-sm text-white-900 dark:text-white ">Enviar a: {props.item.order.delivery_addresss}</p>
                 </div>
 
                 <div className="w-2/12">
                     <p className="text-sm text-white-900 dark:text-white ">Calificar</p>
                     <p className="text-lg text-white-900 dark:text-white font-semibold mb-3 ">{priceFormatterCOP.format(parseFloat(props.item.price))}</p>
+                    
+                    
     
                 </div>
 
