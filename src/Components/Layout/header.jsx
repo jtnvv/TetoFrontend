6b3c8298b4b784be34e724dcd691996a341b07b8
Juan Acevedo <juan.acevedo9805@gmail.com>
2024-05-16 16:48:38 -0500

    Header responsive

--- src/Components/Layout/header.jsx
+++ src/Components/Layout/header.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
-import { useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { FaFileUpload, FaShoppingCart, FaSearch, FaUserCircle, FaHeart, FaSignOutAlt } from "react-icons/fa";
+import { FaFileUpload, FaShoppingCart, FaSearch, FaUserCircle, FaHeart, FaSignOutAlt, FaBars } from "react-icons/fa";
 import { onLogout } from "../../api/auth";
 import { useDispatch } from "react-redux";
 import ShoppingCart from "../Shopping Cart/shopping-cart";
@@ -10,10 +9,10 @@ import { unauthenticateUser } from '../../redux/slices/authSlice.js'
 export default function Header() {
 
   const [isShoppingCartOpen, setIsShoppingCartOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const { role } = useSelector(state => state.auth);
-  const { isAuth } = useSelector(state => state.auth)
-  const location = useLocation()
+  const { isAuth } = useSelector(state => state.auth);
   const dispatch = useDispatch();
   let link;
 
@@ -36,8 +35,10 @@ export default function Header() {
   };
 
   return (
-    <nav className="bg-brand-6 p-3 font-default">
+    <nav className="bg-brand-6 p-3 font-default sticky top-0 w-full z-50 min-h-24">
       <div className="mx-auto flex justify-between items-center">
+        
+        {/* Logo */}
         <div className="flex items-center font-logo cursor-pointer" onClick={() => window.location.href = "/"}>
           <img src="/favicon.svg" alt="Logo" className="h-14 w-14 mr-2" />
           {role === 'brand' ? (<>
@@ -46,62 +47,85 @@ export default function Header() {
               <span className="text-brand-1 font-semibold text-3xl">marcas</span> </div></>) : (<span className="text-brand-1 font-semibold text-5xl">TETO</span>)}
 
         </div>
-        <div className="responsive:flex responsive:items-center responsive:space-x-4">
+
+        <button className={"responsive:hidden p-0 focus:outline-none transition-transform duration-700" + (!isMobileMenuOpen && (" rotate-180"))} onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} >
+          <FaBars size="2em" />
+        </button>
+
+        {/* Menú */}
+        <div className={"h-screen responsive:h-full responsive:static absolute right-0 left-0 top-24 responsive:bg-transparent bg-brand-6 p-5 flex responsive:flex-row flex-col responsive:space-y-0 space-y-5 items-center responsive:space-x-4 transition-transform duration-500 ease-in-out responsive:translate-x-0" + (!isMobileMenuOpen && (" translate-x-[110%]"))} >
 
           {role === 'brand' ? (
             <>
-              <a href="/about-us" className="text-brand-1 hover:text-gray-300 ">Sobre Nosotros</a>
-              <a href="/product-register" className='flex items-center space-x-2'>
+              <a href="/about-us" className="flex text-brand-1 hover:text-gray-300 responsive:w-auto w-full justify-center">Sobre Nosotros</a>
+              <a href="/product-register" className='flex items-center space-x-2 responsive:w-auto w-full justify-center'>
                 <FaFileUpload color="white" size="2em" />
                 <span className="text-brand-1 hover:text-gray-300">Agregar productos</span>
               </a>
 
-              <a href="/brandpage-profile" className='flex items-center space-x-2'>
+              <a href="/brandpage-profile" className='flex items-center space-x-2 responsive:w-auto w-full justify-center'>
                 <FaShoppingCart color="white" size="2em" />
                 <span className="text-brand-1 hover:text-gray-300">Pedidos</span>
               </a>
 
-              <a href={link} className="flex items-center space-x-2">
-                <FaUserCircle color="white" size="2em" className="pl-2" />
+              <a href={link} className="flex items-center space-x-2 responsive:w-auto w-full justify-center">
+                <FaUserCircle color="white" size="2em" />
                 <span className="text-brand-1 hover:text-gray-300">Tu marca de ropa</span>
               </a>
+
               {isAuth && (
-                <button onClick={handleLogout} className="p-0 m-0">
-                  <FaSignOutAlt color="white" size="2em" className="pr-2" />
+                <button onClick={handleLogout} className="flex items-center p-0 bg-brand-6 responsive:w-auto w-full justify-center">
+                  <FaSignOutAlt color="white" size="2em" />
+                  <span className="responsive:hidden pl-2">Cerrar sesión</span>
                 </button>
               )}
 
-            </>) :
+            </>
+            ) : (
+            <>
+              <a href="/search" className="flex responsive:hidden responsive:w-auto w-full justify-center">
+                <button className="flex bg-brand-1 bg-opacity-25 rounded-full pt-2 pb-2 items-center justify-center">
+                  <p className="text-xs text-brand-1">Buscar producto por marca, colección</p><FaSearch color="white" size="1.5em" className="pl-2" />
+                </button>
+              </a>
+              <a href="/" className="flex text-brand-1 hover:text-gray-300 responsive:w-auto w-full justify-center">Inicio</a>
+              <a href="/brand-search" className="flex text-brand-1 hover:text-gray-300 responsive:w-auto w-full justify-center">Marcas</a>
+              <a href="/about-us" className="flex text-brand-1 hover:text-gray-300 responsive:w-auto w-full justify-center">Sobre Nosotros</a>
+              <a href="/search" className="hidden responsive:block">
+                <button className="flex bg-brand-1 bg-opacity-25 rounded-full pt-2 pb-2 items-center justify-center">
+                  <p className="text-xs text-brand-1">Buscar producto por marca, colección</p><FaSearch color="white" size="1.5em" className="pl-2" />
+                </button>
+              </a>
 
-            (<><a href="/" className="text-brand-1 hover:text-gray-300">Inicio</a>
-              <a href="/brand-search" className="text-brand-1 hover:text-gray-300">Marcas</a>
-              <a href="/about-us" className="text-brand-1 hover:text-gray-300">Sobre Nosotros</a>
-              <a href="/search"><button className="flex bg-brand-1 bg-opacity-25 rounded-full pt-2 pb-2 items-center justify-center"><p className="text-xs text-brand-1">Buscar producto por marca, colección</p><FaSearch color="white" size="1.5em" className="pl-2" /></button></a>
+              <div className="flex responsive:space-x-0 space-x-5 responsive:w-auto w-full justify-center">
+                <a href={link}><FaUserCircle color="white" size="2em" className="responsive:pl-2" /></a>
+                <a href="/favorites"><FaHeart color="white" size="2em" className="responsive:pl-2" /></a>
+                
+                <div className="static" id="flex shopping-cart-icon" >
+                  <a
+                    className="cursor-pointer"
+                    onClick={(event) => {
+                      handleIsShoppingCartOpen(!isShoppingCartOpen);
+                      event.preventDefault();
+                    }}
+                    id="shopping-cart-icon"
+                    href="/shopping-cart">
+                    <FaShoppingCart color="white" size="2em" className="responsive:pl-2 responsive:mr-2" id="shopping-cart-icon" />
+                  </a>
+                  {isShoppingCartOpen && (
+                    <ShoppingCart showShoppingCart={handleIsShoppingCartOpen} />
+                  )}
+                </div>
 
-
-              <a href={link}><FaUserCircle color="white" size="2em" className="pl-2" /></a>
-              <a href="/favorites"><FaHeart color="white" size="2em" className="pl-2" /></a>
-              <div className="static" id="shopping-cart-icon" >
-                <a
-                  className="cursor-pointer"
-                  onClick={(event) => {
-                    handleIsShoppingCartOpen(!isShoppingCartOpen);
-                    event.preventDefault();
-                  }}
-                  id="shopping-cart-icon"
-                  href="/shopping-cart">
-                  <FaShoppingCart color="white" size="2em" className="pl-2 mr-2" id="shopping-cart-icon" />
-                </a>
-                {isShoppingCartOpen && (
-                  <ShoppingCart showShoppingCart={handleIsShoppingCartOpen} />
+                {isAuth && (
+                  <button onClick={handleLogout} className="responsive:block p-0 m-0 bg-brand-6">
+                    <FaSignOutAlt color="white" size="2em" className="responsive:pl-2" />
+                  </button>
                 )}
               </div>
-              {isAuth && (
-                <button onClick={handleLogout} className="p-0 m-0">
-                  <FaSignOutAlt color="white" size="2em" className="pr-2" />
-                </button>
-              )}
-            </>)}
+             
+            </>
+          )}
         </div>
       </div>
     </nav>
