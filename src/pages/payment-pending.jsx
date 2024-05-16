aaaef0d7d931478f8d8623d31956b7d0367bc63a
Juan Acevedo <juan.acevedo9805@gmail.com>
2024-05-15 20:31:31 -0500

    Create return payment pages

---
+++ src/pages/payment-pending.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+import Layout from "../Components/Layout/layout";
+
+export default function PaymentPending() {
+    const params = useParams();
+
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
